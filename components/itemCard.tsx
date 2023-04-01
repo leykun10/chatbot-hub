6a0c17335df21fe056bf2c391912b83933f13000
leykun10 <leykunejigue@gmail.com>
2023-04-02 02:16:49 +0300

    added dummy content

--- components/itemCard.tsx
+++ components/itemCard.tsx
@@ -1,16 +1,28 @@
 
 export function ItemCard (){
 
+    // grid  rounded-lg grid-rows-3 
     return (
-     <div className="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-                <img className="object-center object-cover h-auto w-full" src="https://sep.yimg.com/ca/I/paulgraham_2239_0" alt="photo">
-                </img>
-            </div>
-            <div className="text-center py-8 sm:py-6">
-                <p className="text-xl text-gray-700 font-bold mb-2">Paul Graham GPT</p>
-                <p className="text-base text-gray-400 font-normal">paul graham chatbot</p>
-            </div>
+      <div className="flex flex-col bg-slate-100 w-72 h-64">
+        <div className="mt-4">
+          <img
+            className="w-12 h-12 ml-4 p-1 rounded-full ring-gray-300 dark:ring-gray-500"
+            src="https://sep.yimg.com/ca/I/paulgraham_2239_0"
+            alt="Bordered avatar"
+          ></img>
+          <div className="ml-4 mt-4">
+          <h1 className="font-bold text-base">Paul GPT</h1>
         </div>
-    )
+        </div>
+        <div className="ml-4 mt-4">
+          <p className="mt-0 mb-4 text-base font-light leading-relaxed">
+            AI-powered search & chat for Paul Graham's essays.
+          </p>
+        </div>
+        <div className="flex flex-row justify-evenly">
+       <div className="">Rating</div>     
+       <div className="">Link</div>     
+        </div>
+      </div>
+    );
 }
