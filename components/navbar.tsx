47bd7bdf840ca9673d9dc69aedcb8492763360c9
leykun10 <leykunejigue@gmail.com>
2023-04-02 02:16:14 +0300

    name renamed and set width

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 
 export const Navbar: FC = () => {
   return (
-    <div className="flex h-[60px] border-b border-gray-300 py-2 px-8 items-center justify-center">
+    <div className="flex h-[60px] w-auto border-b border-gray-300 py-2 px-8 items-center justify-center">
       <div className="font-bold text-2xl flex items-center">
-        Chatbot Catalog 
+        Chatbot Hub 
       </div>
     </div>
   );
