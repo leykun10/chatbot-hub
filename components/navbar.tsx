67919781a675ba4b6e8bf09c45c1e8c52f213014
leykun10 <leykunejigue@gmail.com>
2023-04-03 01:09:16 +0300

    updated font style and height

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,10 +2,11 @@ import { FC } from "react";
 
 export const Navbar: FC = () => {
   return (
-    <div className="flex h-[60px] w-auto border-b border-gray-300 py-2 px-8 items-center justify-center">
-      <div className="font-bold text-2xl flex items-center">
+    <div className="flex h-[70px] w-auto border-b border-gray-300 py-2 px-8 items-center justify-center">
+      <div className="text-3xl font-serif flex items-center">
         Chatbot Hub 
       </div>
+      {/* <div className="ml-60">Create your own</div> */}
     </div>
   );
 };
