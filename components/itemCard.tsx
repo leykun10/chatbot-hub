7d55f77fba75aee1bd1cff4b659f9b62207ba47a
leykun10 <leykunejigue@gmail.com>
2023-04-03 01:14:14 +0300

    added props

--- components/itemCard.tsx
+++ components/itemCard.tsx
@@ -1,9 +1,10 @@
-
-export function ItemCard (){
+'use client'
+import {MdGrade  } from 'react-icons/md';
+export function ItemCard ({name,description,stars}){
 
     // grid  rounded-lg grid-rows-3 
     return (
-      <div className="flex flex-col bg-slate-100 w-72 h-64">
+      <div className="flex flex-col bg-slate-100 w-72 h-64 hover:bg-white hover:cursor-pointer">
         <div className="mt-4">
           <img
             className="w-12 h-12 ml-4 p-1 rounded-full ring-gray-300 dark:ring-gray-500"
@@ -11,17 +12,17 @@ export function ItemCard (){
             alt="Bordered avatar"
           ></img>
           <div className="ml-4 mt-4">
-          <h1 className="font-bold text-base">Paul GPT</h1>
+          <h1 className="font-bold text-base">{name}</h1>
         </div>
         </div>
         <div className="ml-4 mt-4">
           <p className="mt-0 mb-4 text-base font-light leading-relaxed">
-            AI-powered search & chat for Paul Graham's essays.
+            {description}
           </p>
         </div>
-        <div className="flex flex-row justify-evenly">
-       <div className="">Rating</div>     
-       <div className="">Link</div>     
+        <div className="flex flex-row justify-start p-4">
+       <div className=""><MdGrade/><h1>{stars}</h1></div>     
+       <div className=""></div>     
         </div>
       </div>
     );
