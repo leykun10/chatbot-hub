1f1678432dd7ea7561bf67a9fe62266c599c8f78
leykun10 <leykunejigue@gmail.com>
2023-04-03 01:15:00 +0300

    added category queries with supabase and map rendering

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,36 +1,16 @@
+import { supabase } from "@/utils/superbase";
+import { SidebarItem } from "./sidebarItem";
 
-export function Sidebar(){
+
+export async function Sidebar(){
+const {error,data} = await supabase.from('category').select('name,id')
 
 return (
-  <div className="flex flex-col w-60 divide-y">
-    <div className="h-12">
+  <div className="flex flex-col w-60 divide-y text-cyan-500">
+    <div className="h-12 text-black">
       <h2 className="font-bold text-start ml-10">Categories</h2>
     </div>
-    <div className="h-10 text-start">
-      <h1 className="ml-10  hover:underline">
-        <a href="/podcasts">Podcasts</a>
-      </h1>
-    </div>
-    <div className="h-10 text-start">
-      <h1 className="ml-10 hover:underline">
-        <a href="/start-up">Start Up</a>
-      </h1>
-    </div>
-    <div className="h-10 text-start">
-      <h1 className="ml-10 hover:underline">
-        <a href="/psychology">Psychology</a>
-      </h1>
-    </div>
-    <div className="h-10 text-start">
-      <h1 className="ml-10 hover:underline">
-        <a href="/finance">Finance</a>
-      </h1>
-    </div>
-    <div className="h-10 text-start">
-      <h1 className="ml-10 hover:underline">
-        <a href="/health">Health</a>
-      </h1>
-    </div>
+    {data.map((item) => (<SidebarItem category={item.name} key={item.id} />))}
   </div>
 );
 }
