154fba259392722fa1c2f125bc38bf4b92999185
leykun10 <leykunejigue@gmail.com>
2023-04-03 01:13:46 +0300

    aaded supabase query and map rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,18 @@ import { Footer } from "@/components/footer";
 import { ItemCard } from "@/components/itemCard";
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
+import { supabase } from "@/utils/superbase";
+export default async function Home() {
 
-export default function Home() {
+  const { data, error } = await supabase.from('chatbot').select('*')
   return (
     <div className="flex h-screen flex-col">
       <Navbar />
       <main className="flex flex-1 mt-10">
         <div className="flex flex-col lg:flex-row w-screen lg:ml-80 mb-10 mt-8">
-          <Sidebar />
+          <Sidebar/>
           <div className="flex gap-6 pb-10  flex-wrap flex-row h-max w-3/4  ml-8">
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-           
+            {data?.map((item) => <ItemCard key={item.id} name={item.name} description={item.description} stars={item.rating} />)}
           </div>
         </div>
       </main>
