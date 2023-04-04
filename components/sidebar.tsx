811cb38c643525ed71f232abd8c84de1e628bf21
leykun10 <leykunejigue@gmail.com>
2023-04-04 23:29:22 +0300

    added some background color and shadow

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,9 +4,8 @@ import { SidebarItem } from "./sidebarItem";
 
 export async function Sidebar(){
 const {error,data} = await supabase.from('category').select('name,id')
-
 return (
-  <div className="flex flex-col w-60 divide-y text-cyan-500">
+  <div className="flex flex-col w-60 divide-y text-black bg-slate-100 shadow-sm">
     <div className="h-12 text-black">
       <h2 className="font-bold text-start ml-10">Categories</h2>
     </div>
