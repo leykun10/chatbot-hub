223487dd316ca5d5e8fd67df0b02b9f84c9117a0
leykun10 <leykunejigue@gmail.com>
2023-04-04 23:28:15 +0300

    added custom image for each bots

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,15 @@ import { supabase } from "@/utils/superbase";
 export default async function Home() {
 
   const { data, error } = await supabase.from('chatbot').select('*')
+  console.log(data)
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-screen flex-col bg-slate-100">
       <Navbar />
       <main className="flex flex-1 mt-10">
         <div className="flex flex-col lg:flex-row w-screen lg:ml-80 mb-10 mt-8">
           <Sidebar/>
           <div className="flex gap-6 pb-10  flex-wrap flex-row h-max w-3/4  ml-8">
-            {data?.map((item) => <ItemCard key={item.id} name={item.name} description={item.description} stars={item.rating} />)}
+            {data?.map((item) => <ItemCard key={item.id} name={item.name} description={item.description} stars={item.rating} imgUrl={item.image} />)}
           </div>
         </div>
       </main>
--- components/itemCard.tsx
+++ components/itemCard.tsx
@@ -1,26 +1,33 @@
 'use client'
 import {MdGrade  } from 'react-icons/md';
-export function ItemCard ({name,description,stars}){
 
-    // grid  rounded-lg grid-rows-3 
+function checkForLength(str:string){
+    if(str.length > 100  ){
+        return str.slice(0, 100- 3) + "...";
+    }else{
+        return str
+    }
+}
+
+export function ItemCard ({name,description,stars,imgUrl}:{name:string,description:string,stars:number,imgUrl:string}){
     return (
-      <div className="flex flex-col bg-slate-100 w-72 h-64 hover:bg-white hover:cursor-pointer">
+      <div className="flex flex-col bg-slate-100 shadow-lg w-72 h-72 hover:bg-white hover:cursor-pointer">
         <div className="mt-4">
           <img
-            className="w-12 h-12 ml-4 p-1 rounded-full ring-gray-300 dark:ring-gray-500"
-            src="https://sep.yimg.com/ca/I/paulgraham_2239_0"
+            className="w-16 h-16 ml-4 p-1 rounded-full ring-gray-300 dark:ring-gray-500 object-fill"
+            src={imgUrl}
             alt="Bordered avatar"
           ></img>
           <div className="ml-4 mt-4">
           <h1 className="font-bold text-base">{name}</h1>
         </div>
         </div>
-        <div className="ml-4 mt-4">
-          <p className="mt-0 mb-4 text-base font-light leading-relaxed">
+        <div className="p-4 h-20">
+          <p className=" mb-4 text-base font-light leading-relaxed">
             {description}
           </p>
         </div>
-        <div className="flex flex-row justify-start p-4">
+        <div className="flex flex-row justify-start p-6">
        <div className=""><MdGrade/><h1>{stars}</h1></div>     
        <div className=""></div>     
         </div>
