1d61e47ff54da3b1d5f25d4a7212f2a6d875674b
leykun10 <leykunejigue@gmail.com>
2023-04-11 02:02:37 +0300

    added chatbot page

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,7 @@
+
+export default function Chatbot() {
+    return (<div className="flex items-center justify-center h-screen w-screen">
+      
+      <div className="flex"> <h1 className="text-4xl">Coming Soon</h1></div>  
+    </div>)
+}
