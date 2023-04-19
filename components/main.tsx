c7f307e6ef23ea160616ac92c56103e7d3483dca
leykun10 <leykunejigue@gmail.com>
2023-04-19 13:02:15 +0300

    added types

--- components/main.tsx
+++ components/main.tsx
@@ -1,8 +1,8 @@
 import { ItemCard } from "./itemCard"
-export default function Main({data}){
+export default function Main({data}:{data:Array<any>}){
 return (
 < div className="flex gap-6 pb-10  flex-wrap flex-row h-max w-3/4  ml-8">
-            {data?.map((item) => <ItemCard key={item.id} name={item.name} description={item.description} stars={item.rating} imgUrl={item.image} url={item.url}/>)}
+            {data?.map((item:any) => <ItemCard key={item.id} name={item.name} description={item.description} stars={item.rating} imgUrl={item.image} url={item.url}/>)}
  </div>
 )
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,12 +5,14 @@ import { SidebarItem } from "./sidebarItem";
 export default async function Sidebar(){
 const {error,data} = await supabase.from('category').select('name')
 
+if (error!=null) return (<div></div>)
+
 return (
   <div className="flex flex-col w-60 h-2/3 divide-y text-black ring hover:ring-emerald-400 ring-gray-100 ring-offset-4 bg-gray-50 px-6 py-6 rounded-md">
     <div className="h-12 text-black">
       <h2 className="font-bold text-start ml-10">Categories</h2>
     </div>
-    {data.map((item) => (<SidebarItem category={item.name} key={item.name} />))}
+    {data!.map((item) => (<SidebarItem category={item.name} key={item.name} />))}
   </div>
 );
 }
