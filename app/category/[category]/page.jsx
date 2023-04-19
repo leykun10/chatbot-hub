1dfa1baa302417e2f7904329091a98c70e0425a0
leykun10 <leykunejigue@gmail.com>
2023-04-19 12:09:30 +0300

    added api response reformating logic for compatibilty

--- app/category/[category]/page.jsx
+++ app/category/[category]/page.jsx
@@ -1,22 +1,29 @@
-import { ItemCard } from "@/components/itemCard"
 import { Sidebar } from "@/components/sidebar"
 import { supabase } from "@/utils/superbase"
+import Main from "@/components/main"
 export default  async function Page({params}) {
-//   const { data, error } = await supabase
-// .from('chatbot')
-// .select(' *, members!inner ( * ) ')
   
- let { data, error } = await supabase
+const { data, error } = await supabase
   .from('chatbot_category')
   .select('chatbot(*)').filter('chatbot_category', 'eq', params.category)
 
-  console.log(data)
+
 
 return(<div className="flex flex-col lg:flex-row w-screen lg:ml-80 mb-10 mt-8">
           <Sidebar/>
-          <div className="flex gap-6 pb-10  flex-wrap flex-row h-max w-3/4  ml-8">
-            {data?.map((item) => <ItemCard key={item.chatbot.id} name={item.chatbot.name} description={item.chatbot.description} stars={item.chatbot.rating} imgUrl={item.chatbot.image} url={item.chatbot.url}/>)}
-          </div>
+          {error != null? <div></div>: <Main data={data.map((item) => {
+  const { id, created_at, name, url, description, image, rating, repo_link } = item.chatbot;
+  return {
+    id,
+    created_at,
+    name,
+    url,
+    description,
+    image,
+    rating,
+    repo_link,
+  };
+})} /> }
   </div>)
 }
 
