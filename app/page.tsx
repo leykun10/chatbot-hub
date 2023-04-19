f147eb345b5ac8d34d1c9de56da2da351f0da677
leykun10 <leykunejigue@gmail.com>
2023-04-19 12:09:52 +0300

    added error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/footer";
 import { ItemCard } from "@/components/itemCard";
+import Main from "@/components/main";
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 import { supabase } from "@/utils/superbase";
@@ -7,14 +8,13 @@ import { supabase } from "@/utils/superbase";
 export const revalidate = 60; // revalidate this page every 60 seconds
 export default async function Home() {
 
-  const { data, error } = await supabase.from('chatbot').select('*')
-  console.log(data)
+  const {data,error} = await supabase.from('chatbot').select('*')
+  
   return (
         <div className="flex flex-col lg:flex-row w-screen lg:ml-80 mb-10 mt-8">
           <Sidebar/>
-          <div className="flex gap-6 pb-10  flex-wrap flex-row h-max w-3/4  ml-8">
-            {data?.map((item) => <ItemCard key={item.id} name={item.name} description={item.description} stars={item.rating} imgUrl={item.image} url={item.url}/>)}
-          </div>
+          {error!=null?<div></div>:
+         <Main  data={data}/>}
         </div>
   );
 }
