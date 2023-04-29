98443ebed750e4b4b2bf5cdc234efc5d3143da17
leykun10 <leykunejigue@gmail.com>
2023-04-30 00:35:55 +0300

    added blog item in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,15 +6,14 @@ export const Navbar: FC = () => {
     <div className="flex gap-x-96 flex-col lg:flex-row lg:ml-80 items-center">
       <div className="flex gap-2 flex-2 flex-row text-4xl font-serif items-center">
         <img src="/cool_logo_pr.png" width={80} height={80}></img>
-        <a href="/">
+        <a className="text-emerald-500" href="/">
         Chatbot Hub 
         </a>
       </div>
-      <div className="flex flex-row items-center gap-8">
+      <div className="flex flex-row items-center gap-8 ">
       {/* <div className="flex hover:underline text-center"><a href="/about">About</a></div> */}
-      {/* <div className="flex hover:underline text-center"><a href="/blog">Blog</a></div> */}
-      <div className="flex hover:underline text-center"><a href="https://www.chatbase.co/?via=leykun" target="_blank">Create your own chatbot</a></div>
-      {/* <div className="flex hover:underline text-center"><a href="/submit_chatbot">Submit a chatbot</a></div> */}
+      <div className="flex hover:underline text-center hover:text-emerald-400 "><a href="/blog">Blog</a></div>
+      <div className="flex hover:underline text-center hover:text-emerald-400"><a href="https://www.chatbase.co/?via=leykun" target="_blank">Create your own chatbot</a></div>
       <div className="flex">
         <Modal/>
       </div>
