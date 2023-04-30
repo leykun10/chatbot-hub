8f5be5cbd5fdeb9b9cf5ac6f972c4a31e7aee466
leykun10 <leykunejigue@gmail.com>
2023-04-30 23:43:16 +0300

    added horizontal separator and added mobile view

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,8 +9,8 @@ export default async function Blog() {
    {/* title for all posts */}
     <h1 className="text-4xl">All Posts</h1>
 </div>
-
-<div className="flex gap-4 flex-wrap lg:flex-row">
+<hr className="h-px my-8 bg-gray-600 border-0 dark:bg-gray-700"></hr>
+<div className="flex gap-4 flex-wrap flex-col lg:flex-row">
 
     {metadatas?.map(data=>(<BlogCard key={data.slug} data={data}/>))}
 </div>
